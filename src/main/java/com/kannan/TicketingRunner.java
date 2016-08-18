c9a1c84fe242b2d266df56149418c11599c43901
kannan <kannan@example.com>
2016-08-18 19:02:01 -0400

    TicketingRunner updated

--- src/main/java/com/kannan/TicketingRunner.java
+++ src/main/java/com/kannan/TicketingRunner.java
@@ -19,11 +19,19 @@ public class TicketingRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Loading MockData using MockDataLoader");
         loader.loadData();
+        System.out.println("Loading Complete");
+        System.out.println("Number of Seats Available for Level 1 - Orchestra :");
         System.out.println(ticketService.numSeatsAvailable(Optional.of(1)));
-        SeatHold seatHold = ticketService.findAndHoldSeats(5, Optional.of(1), Optional.of(1), "foo");
+        System.out.println("Holding 5 seats for a customer - cust@test.com");
+        SeatHold seatHold = ticketService.findAndHoldSeats(5, Optional.of(1), Optional.of(1), "cust@test.com");
+        System.out.println("Number of Seats Available for Level 1 - Orchestra - After Seat Hold is done :");
         System.out.println(ticketService.numSeatsAvailable(Optional.of(1)));
-        String id = ticketService.reserveSeats(seatHold.getSeatHoldId(), "foo");
+        System.out.println("Reservation being done for the seats held");
+        String id = ticketService.reserveSeats(seatHold.getSeatHoldId(), "cust@test.com");
+        System.out.println("Reservation complete!");
+        System.out.println("Reservation ID : ");
         System.out.println(id);
     }
 }
